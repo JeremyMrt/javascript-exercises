105c68d890809e0d6bec84210edccf2b96d304cf
Jérémy Martin <114619755+JeremyMrt@users.noreply.github.com>
2022-12-01 15:59:03 +0100

    findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  
+    people.forEach(checkDeath);
+
+    function checkDeath(people) {
+        if (people.yearOfDeath === undefined) {
+                people.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    
+    const ordered = people.sort((a,b) => a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1)
+
+    console.log(ordered[0]);
+    return ordered[0];
+
 
 };
 
