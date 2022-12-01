d3b5a173d1e2142e46eab67376864ab8f4717d75
Jérémy Martin <114619755+JeremyMrt@users.noreply.github.com>
2022-12-01 14:39:44 +0100

    Fibonnaci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fiboStart = [0, 1, 1, 2, 3]
+    if (number < 0) return 'OOPS'
+    if (number < 5) return fiboStart[number]
+    for (i=5 ; i <= number ; i++) {
+        fiboStart.push(fiboStart[i-1] + fiboStart[i - 2])
+    }
+    return fiboStart[number]
 };
 
 // Do not edit below this line
